948fc75f656b3928bf86a1d08d92047c3fa8ecf8
huangxieqiang <huangxieqiang@stbl.cc>
2017-04-14 11:00:56 +0800

    提交.net 版本代码

---
+++ src/asp.net/HXQ.PT.Web/Controllers/TestController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HXQ.PT.Web.Controllers
+{
+    public class TestController : Controller
+    {
+
+        public ActionResult Query(int ms = 0)
+        {
+            if (ms > 0)
+            {
+                System.Threading.Thread.Sleep(ms);
+            }
+            string strNow = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff");
+            return Content(strNow);
+        }
+
+        public ActionResult QueryEx(int ms = 0)
+        {
+            if (ms > 0)
+            {
+                System.Threading.Thread.Sleep(ms);
+            }
+
+            string strNow = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff");
+            var result = new ResultModel()
+            {
+                nowtime = strNow,
+                ms = ms
+            };
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public class ResultModel
+        {
+            public string nowtime { get; set; }
+            public int ms { get; set; }
+        }
+    }
+}
---
+++ src/asp.net/HXQ.PT.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HXQ.PT.Web.MvcApplication" Language="C#" %>
